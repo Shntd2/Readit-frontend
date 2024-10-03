a7a6cd2fbef552ef8f299d7d679518654c62269e
shntd2 <154740265+Shntd2@users.noreply.github.com>
2024-10-03 14:14:30 +0000

    Uploaded ico of Readit app

--- readit-app/src/App.jsx
+++ readit-app/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+// import { AuthContext } from "./contexts/AuthContext";
 import './App.css'
 
 function App() {
